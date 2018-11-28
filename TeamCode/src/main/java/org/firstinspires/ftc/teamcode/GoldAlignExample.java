e94e6b947fd7b3be09b3e197dcdce214edfd55aa
andrewzheng <a.zheng.4801@gmail.com>
2018-11-28 18:26:51 -0500

    Add Vuforia Autonomous code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoldAlignExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoldAlignExample.java
@@ -105,3 +105,5 @@ public class GoldAlignExample extends OpMode
     }
 
 }
+
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TileRunnerDepotAuto.java
@@ -0,0 +1,204 @@
+package org.firstinspires.ftc.teamcode.dogecv;
+
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.DogeCV;
+import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+
+@Autonomous(name="TileRunnerDepotAuto", group="DogeCV")
+
+public class TileRunnerDepotAuto extends OpMode {
+    // Detector object
+    private GoldAlignDetector detector;
+    private ElapsedTime runtime = new ElapsedTime();
+    int LiftPower = 1;
+    // left = -1, center = 0, right = 1
+    int position = 0;
+
+    DcMotor upMotor;
+    DcMotor downMotor;
+    DcMotor flDrive;
+    DcMotor frDrive;
+    DcMotor rlDrive;
+    DcMotor rrDrive;
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "DogeCV 2018.0 - Gold Align Example");
+
+        // Set up detector
+        detector = new GoldAlignDetector(); // Create detector
+        detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance()); // Initialize it with the app context and camera
+        detector.useDefaults(); // Set detector to use default settings
+
+        // Optional tuning
+        detector.alignSize = 100; // How wide (in pixels) is the range in which the gold object will be aligned. (Represented by green bars in the preview)
+        detector.alignPosOffset = 0; // How far from center frame to offset this alignment zone.
+        detector.downscale = 0.4; // How much to downscale the input frames
+
+        detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
+        //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
+        detector.maxAreaScorer.weight = 0.005; //
+
+        detector.ratioScorer.weight = 5; //
+        detector.ratioScorer.perfectRatio = 1.0; // Ratio adjustment
+
+        flDrive = hardwareMap.get(DcMotor.class, "fl_drive");
+        frDrive = hardwareMap.get(DcMotor.class, "fr_drive");
+        rlDrive = hardwareMap.get(DcMotor.class, "rl_drive");
+        rrDrive = hardwareMap.get(DcMotor.class, "rr_drive");
+        upMotor = hardwareMap.get(DcMotor.class, "up_motor");
+        downMotor = hardwareMap.get(DcMotor.class, "down_motor");
+
+        flDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rlDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rrDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        upMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        downMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        flDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rlDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rrDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        upMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        downMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        flDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rlDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rrDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        upMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        downMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        flDrive.setDirection(DcMotor.Direction.FORWARD);
+        frDrive.setDirection(DcMotor.Direction.REVERSE);
+        rlDrive.setDirection(DcMotor.Direction.FORWARD);
+        rrDrive.setDirection(DcMotor.Direction.REVERSE);
+        upMotor.setDirection(DcMotor.Direction.FORWARD);
+        downMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        int currentUpPos = upMotor.getCurrentPosition();
+        int currentDownPos = downMotor.getCurrentPosition();
+
+        upMotor.setTargetPosition(currentUpPos);
+        downMotor.setTargetPosition(currentDownPos);
+
+        upMotor.setPower(LiftPower);
+        downMotor.setPower(LiftPower);
+
+
+    }
+
+    /*
+     * Code to run REPEATEDLY when the driver hits INIT
+     */
+    @Override
+    public void init_loop() {
+
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY when the driver hits PLAY
+     */
+    @Override
+    public void loop() {
+        while (runtime.milliseconds() < 30000) {
+            while (runtime.milliseconds() < 3000) {
+                upMotor.setPower(-1);
+                downMotor.setPower(1);
+            }
+            while (3000 < runtime.milliseconds() && runtime.milliseconds() < 6000) {
+                detector.enable(); // Start the detector!
+                if (detector.getAligned()) {
+                    position = 0;
+                }
+                else if (detector.getXPosition() < -20) {
+                    position = -1;
+                }
+                else {
+                    position = 1;
+                }
+                telemetry.addData("IsAligned", detector.getAligned()); // Is the bot aligned with the gold mineral?
+                telemetry.addData("X Pos", detector.getXPosition()); // Gold X position.
+            }
+            if (position == -1) {
+                //turn left
+                encoderDrive(1,-20,20,-20,20);
+            }
+            if (position == 0) {
+                //move forward
+                encoderDrive(1, 20, 20, 20, 20);
+            }
+            if (position == 1) {
+                //turn right
+                encoderDrive(1,20,-20,20,-20);
+            }
+        }
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+        // Disable the detector
+        detector.disable();
+    }
+
+    public void encoderDrive ( double speed, int flDrivePos, int frDrivePos, int rlDrivePos,
+                               int rrDrivePos){
+        flDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rlDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rrDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        flDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rlDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rrDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        flDrivePos = flDrivePos * (1125 / ((45 / 35) * (32))) + flDrive.getCurrentPosition();
+        frDrivePos = frDrivePos * (1125 / ((45 / 35) * (32))) + frDrive.getCurrentPosition();
+        rlDrivePos = rlDrivePos * (1125 / ((45 / 35) * (32))) + rlDrive.getCurrentPosition();
+        rrDrivePos = rrDrivePos * (1125 / ((45 / 35) * (32))) + rrDrive.getCurrentPosition();
+
+        flDrive.setTargetPosition(flDrivePos);
+        frDrive.setTargetPosition(frDrivePos);
+        rlDrive.setTargetPosition(rlDrivePos);
+        rrDrive.setTargetPosition(rrDrivePos);
+
+        flDrive.setPower(speed);
+        frDrive.setPower(speed);
+        rlDrive.setPower(speed);
+        rrDrive.setPower(speed);
+
+        while (flDrive.isBusy() || frDrive.isBusy() || rlDrive.isBusy() || rrDrive.isBusy()) {
+
+        }
+
+        flDrive.setPower(0);
+        frDrive.setPower(0);
+        rlDrive.setPower(0);
+        rrDrive.setPower(0);
+
+        flDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rlDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rrDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+}
+
