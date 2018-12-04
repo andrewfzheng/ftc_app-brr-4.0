aeb4f2db137ba31de7f92d083cdedb1c5a6573cc
andrewzheng <a.zheng.4801@gmail.com>
2018-12-03 22:51:51 -0500

    Archive old code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoldAlignExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoldAlignExample.java
@@ -32,12 +32,10 @@ package org.firstinspires.ftc.teamcode.dogecv;
 import com.disnodeteam.dogecv.CameraViewDisplay;
 import com.disnodeteam.dogecv.DogeCV;
 import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
-import com.disnodeteam.dogecv.detectors.roverrukus.SamplingOrderDetector;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-
-@TeleOp(name="GoldAlign Example", group="DogeCV")
+@Autonomous(name="GoldAlign Example", group="DogeCV")
 
 public class GoldAlignExample extends OpMode
 {
